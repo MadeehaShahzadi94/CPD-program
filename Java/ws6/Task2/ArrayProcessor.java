9ac9e43c00d64edd8d759243462d34ac43ca73f4
Madeeha Shahzadi <59655757+MadeehaShahzadi94@users.noreply.github.com>
2021-04-04 16:38:26 -0400

    Add files via upload

---
+++ Java/ws6/Task2/ArrayProcessor.java
@@ -0,0 +1,20 @@
+/**********************************************
+Workshop # 6
+Course:<JAV444> - Semester Fall
+Last Name:<Shahzadi>
+First Name:<Madeeha>
+ID:<130638182>
+Section:<NDD>
+This assignment represents my own work in accordance with Seneca Academic Policy.
+Signature: Madeeha Shahzadi
+Date:<submission date> November, 10 2020
+**********************************************/
+package ws6_task2;
+
+
+@FunctionalInterface
+public interface ArrayProcessor {
+	
+      double apply( double[] array );
+      
+}
