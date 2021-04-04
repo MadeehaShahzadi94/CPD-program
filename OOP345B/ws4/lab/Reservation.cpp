895038630ec8eb054005ee8fc395f72810f61401
Madeeha Shahzadi <59655757+MadeehaShahzadi94@users.noreply.github.com>
2021-04-04 17:54:36 -0400

    Add files via upload

---
+++ OOP345B/ws4/lab/Reservation.cpp
@@ -0,0 +1,81 @@
+// Name:Madeeha Shahzadi
+// Seneca Student ID:130638182
+// Seneca email:mshahzadi1@myseneca.ca
+// Date of completion: Febuary 5th 2020
+//
+// I confirm that the content of this file is created by me,
+//   with the exception of the parts provided to me by my professor.
+#include "Reservation.h"
+#include <iomanip>
+using namespace std;
+namespace sdds{
+void Reservation::trim(std::string & str) const
+{
+	size_t i = 0u;
+	for (i = 0u; i < str.length() && str[i] == ' '; ++i);
+	str = str.substr(i);
+
+	for (i = str.length(); i > 0 && str[i - 1] == ' '; --i);
+	str = str.substr(0, i);
+}
+sdds::Reservation::Reservation()
+{
+}
+
+sdds::Reservation::Reservation(const std::string & res)
+{
+	//for reservation id
+	//string point 0 for the string start position
+	size_t poss = 0;
+	//search for : and stop there
+	size_t posE = res.find(':');
+	//remove the space
+	this->trim(res_id = res.substr(poss, posE - poss));
+
+	//for the reservation name
+	poss = posE + 1;
+	posE = res.find(',', poss);
+	this->trim(name_res = res.substr(poss, posE - poss));
+
+	//for the email
+	poss = posE + 1;
+	posE = res.find(',', poss);
+	this->trim(email = res.substr(poss, posE - poss));
+
+	//number of people in party
+	poss = posE + 1;
+	posE = res.find(',', poss);
+	numOfPeople = std::stoi(res.substr(poss, posE - poss));
+
+	//days
+	poss = posE + 1;
+	posE = res.find(',', poss);
+	m_day = std::stoi(res.substr(poss, posE - poss));
+
+	//hours
+	poss = posE + 1;
+	posE = res.find(',', poss);
+	m_hour = std::stoi(res.substr(poss, posE - poss));
+
+}
+
+std::ostream & operator<<(std::ostream & out, const Reservation & res)
+{
+	// TODO: insert return statement here
+	std::string mail = "<" + res.email + ">";
+	out << "Reservation " << res.res_id << ": " << std::setw(10) << std::right << res.name_res << "  " << std::setw(20) << std::left << mail << std::right;
+	if (6 <= res.m_hour && res.m_hour <= 9)
+		out << "    Breakfast ";
+	else if (11 <= res.m_hour && res.m_hour <= 15)
+		out << "    Lunch ";
+	else if (17 <= res.m_hour && res.m_hour <= 21)
+		out << "    Dinner ";
+	else
+		out << "    Drinks ";
+	out << "on day " << res.m_day << " @ " << res.m_hour
+		<< ":00 for " << res.numOfPeople << " people.\n";
+
+	return out;
+
+}
+}
---
+++ OOP345B/ws4/lab/Reservation.h
@@ -0,0 +1,40 @@
+// Name:Madeeha Shahzadi
+// Seneca Student ID:130638182
+// Seneca email:mshahzadi1@myseneca.ca
+// Date of completion: Febuary 5th 2020
+//
+// I confirm that the content of this file is created by me,
+//   with the exception of the parts provided to me by my professor.
+#pragma once
+#ifndef SDDS_RESERVATION_H
+# define SDDS_RESERVATION_H
+
+#include <string>
+namespace sdds {
+	class Reservation {
+		//reservation id: an array of characters
+		std::string res_id = "";
+		//the name on the reservation
+		std::string name_res = "";
+			//the email that can be used to send the confirmation that the reservation can be honored or not
+		std::string email = "";
+			//the number of people in the party
+		size_t numOfPeople = 0;
+			//the day when the party is expected to come(for simplicity, the day is an integer between 1 and 31)
+		size_t m_day = 0;
+			//the hour when the party is expected to come(for simplicity, the hour is an integer between 1 and 24)
+		size_t m_hour = 0;
+		void trim(std::string& str) const;
+	public:
+		//a default constructor
+		Reservation();
+		//one arugument constructor
+		Reservation(const std::string& res);
+		// helper function
+		friend std::ostream& operator<<(std::ostream& out, const Reservation& res);
+	
+	};
+
+}
+#endif // !1
+
