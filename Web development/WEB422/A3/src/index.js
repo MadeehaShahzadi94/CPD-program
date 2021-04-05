9dc2378a0d4aae09a7cb1431ef8b00dc1d5a4ebe
Madeeha Shahzadi <59655757+MadeehaShahzadi94@users.noreply.github.com>
2021-04-04 20:43:36 -0400

    Add files via upload

---
+++ Web development/WEB422/A3/src/index.js
@@ -0,0 +1,28 @@
+/*********************************************************************************
+* WEB422 – Assignment 3
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Madeeha Shahzadi Student ID: 130638182 Date: 26 june 2020
+*
+*
+********************************************************************************/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    ), document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
